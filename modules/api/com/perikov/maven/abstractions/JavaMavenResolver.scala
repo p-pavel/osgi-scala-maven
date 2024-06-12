6f77864dfe6488e34d74bb4155a995cb4b2a9a3f
Pavel Perikov <perikov@gmail.com>
2024-06-12 18:03:42 +0400

    Removed the file that was put here by mistake

--- modules/api/com/perikov/maven/abstractions/JavaMavenResolver.scala
+++
@@ -1,6 +0,0 @@
-package com.perikov.maven.abstractions
-
-trait JavaMavenResolver extends MavenResolver:
-  type F[a] = java.util.concurrent.CompletableFuture[a]
-  type Stream[a] = a match
-    case Byte => java.io.InputStream
