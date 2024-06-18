df3ddacfa471796680d4508d4af68334f4815321
Pavel Perikov <perikov@gmail.com>
2024-06-18 17:52:23 +0400

    removed not-needed files for the branch

--- modules/api/com/perikov/maven/abstractions/0. Introduction.scala
+++
@@ -1,7 +0,0 @@
-package com.perikov.maven
-
-/**
-  * We try to keep this package free of dependencies even on Scala runtime library
-  * to let us provide pure Java interfaces later
-  */
-package object abstractions
--- modules/api/com/perikov/maven/abstractions/0.1 Notation.scala
+++
@@ -1,6 +0,0 @@
-package com.perikov.maven.abstractions
-
-opaque type Unknown = AnyRef
-val Unknown: Unknown = null
-
-type Optional[T] = T | Unknown
